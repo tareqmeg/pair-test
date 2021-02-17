00fba2e7de9fa2ee2810c00145d50234db086219
Mohammad Nofal <m7madmody@gmail.com>
2021-02-17 15:50:24 +0200

    Mohammad changes

--- js/app.js
+++ js/app.js
@@ -7,10 +7,11 @@ alert('welcome ' + greetMasseage );
 let myAge = prompt('Is my age 29?(Yes/No)or (y/n)');
 
 console.log(myAge);
-
+let q1=function(age){
 if (myAge.toUpperCase() === 'YES' || myAge.toLowerCase() === 'y'){
   alert('Thats right you have done good');
 } else if (myAge.toLowerCase() === 'no' || myAge.toLowerCase() === 'n') {
   alert('It is ok it is Hard to gusse some time');
 }
-
+}
+q1(myAge);
